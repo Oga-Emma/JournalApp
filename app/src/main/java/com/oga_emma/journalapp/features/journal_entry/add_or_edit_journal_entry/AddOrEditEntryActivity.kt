63e68d85dea2e63a3c20615f35523bb0b8e8cad8
Oga-Emma <sevenapps2018@gmail.com>
2018-06-26 03:16:22 +0100

    added firebase dependencies

--- app/src/main/java/com/oga_emma/journalapp/features/journal_entry/add_or_edit_journal_entry/AddOrEditEntryActivity.kt
+++ app/src/main/java/com/oga_emma/journalapp/features/journal_entry/add_or_edit_journal_entry/AddOrEditEntryActivity.kt
@@ -2,6 +2,7 @@ package com.oga_emma.journalapp.features.journal_entry.add_or_edit_journal_entry
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.oga_emma.journalapp.R
 
 class AddOrEditEntryActivity : AppCompatActivity() {
 
